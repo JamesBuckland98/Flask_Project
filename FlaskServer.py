4f4937f5ca9834a25dc0d390cc169c2d03797e5f
Saviours Ndau <ndaus@cardiff.ac.uk>
2017-11-16 09:50:33 +0000

    FlaskServer has been tested and is active for deployment

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, redirect, request
 import datetime
-
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
+
+# @app.route('/')
+# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
