354504e0779afaa7e38590853031edef0a0c2621
Veronika Kravchenko <KravchenkoV@cardiff.ac.uk>
2017-11-21 11:53:13 +0000

    added function to render login page

--- FlaskServer.py
+++ FlaskServer.py
@@ -40,9 +40,14 @@ def returnParent():
         return render_template('ParentTemplate.html')
 
 @app.route("/Form", methods=['GET'])
-def returnInheritanceBase():
+def returnForm():
     if request.method == 'GET':
         return render_template('ChildForm.html')
+
+@app.route("/Login", methods=['GET'])
+def returnLogin():
+    if request.method == 'GET':
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
