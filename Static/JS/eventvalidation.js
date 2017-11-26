74a94fa3b752daa1fad4c9b1743e34bd818a7bba
Samuel Okwusogu <OkwusoguS@cardiff.ac.uk>
2017-11-26 22:43:37 +0000

    event validation test 2, empty input strings

--- Static/JS/eventvalidation.js
+++ Static/JS/eventvalidation.js
@@ -1,23 +1,23 @@
-function formEventValidate(){
+function formEventValidate() {
   var date=document.getElementById("date").value;
   var attendance=document.getElementById("Attendance").value;
   var eventType=document.getElementById("eventType").value;
   var ageRange=document.getElementById("AgeRange").value;
 
-  if(date == "mm/dd/yy" || Attendance=="" || eventType=="-- select an option --" || AgeRange ="-- select an option --")
+  if(date == "" || Attendance=="" || eventType=="" || AgeRange ="")
   {
     alert("All fields must be completed.")
     return false;
-//   }
-// else if(isNan(attendance));
-// {
-//   alert(attendance + "is not a valid NUMBER of attendees.")
-//   return false;
-// }
-// else{
-//   alert("Event submitted successfully")
-//   return true;
-// }
-// }
-//unsure still of how to approach age range since multiple can be added - duplicates etc//
-//unsure of how to validate time, so not in the past.//
+  }
+else if(isNan(attendance));
+{
+  alert(attendance + "is not a valid NUMBER of attendees.")
+  return false;
+}
+else{
+  alert("Event submitted successfully")
+  return true;
+}
+}
+// unsure still of how to approach age range since multiple can be added - duplicates etc//
+// unsure of how to validate time, so not in the past.//
