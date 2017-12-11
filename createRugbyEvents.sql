18967f87c782167c3221bcca1617ffc921685de5
Veronika Kravchenko <KravchenkoV@cardiff.ac.uk>
2017-12-11 15:32:54 +0000

    Edited SQL to reset tables and populate them with test data

--- createRugbyEvents.sql
+++ createRugbyEvents.sql
@@ -1,5 +1,5 @@
 
-
+DROP TABLE IF EXISTS Games;
 CREATE TABLE IF NOT EXISTS `Games`(
   `gameID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
   `gameType` TEXT NOT NULL,
@@ -14,14 +14,13 @@ CREATE TABLE IF NOT EXISTS `Events`(
   `Postcode` TEXT NOT NULL
 );
 
-INSERT INTO 'Events'('eventName','Address', 'Postcode') VALUES ('Touch Rugby', 'Cardiff University, Birchwood Road,', '‎CF23 5YB');
-
-
+DROP TABLE IF EXISTS Activities;
 CREATE TABLE IF NOT EXISTS `Activities`(
   `activityID`INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
   `date` DATE NOT NULL,
   `eventName` TEXT NOT NULL,
   `attendance`INTEGER NOT NULL,
+  `EventType` TEXT NOT NULL,
   `Male` INTEGER NOT NULL,
   `Female` INTEGER NOT NULL,
   `gameID` INTEGER NOT NULL,
@@ -30,5 +29,12 @@ CREATE TABLE IF NOT EXISTS `Activities`(
 );
 
 
+INSERT INTO 'Games'('gameID','gameType','ageRange')VALUES('1','Tag','Juniors (8-17)');
+INSERT INTO 'Games'('gameID','gameType','ageRange')VALUES('2','Contact','Seniors (18+)');
+
+INSERT INTO 'Events'(`eventID`,'eventName','Address', 'Postcode') VALUES ('1','Touch Rugby', 'Cardiff University, Birchwood Road,', '‎CF23 5YB');
+INSERT INTO 'Events'(`eventID`,'eventName', 'Address', 'Postcode')VALUES('2','Scarlets Regional Event', 'Parc Pemberton, Llanelli', 'SA14 9UZ');
+INSERT INTO 'Events'(`eventID`,'eventName', 'Address', 'Postcode')VALUES('3','Junior Beach Rugby', 'Barafundle Bay Beach', 'SA71 5LS');
+INSERT INTO 'Events'(`eventID`,'eventName', 'Address', 'Postcode')VALUES('4','Mixed Ability Event', 'Jersey Park', 'SA1 8HN');
 
 -- http://www.sqlitetutorial.net/sqlite-foreign-key/
